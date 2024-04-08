084628d266a90a126c62efba1ec9500a0a51ff4b
blue-ssom <somi5404@naver.com>
2024-04-08 01:19:39 +0000

    implement user registration API end point

--- src/routes/account.js
+++ src/routes/account.js
@@ -17,7 +17,6 @@ router.get('/find-id', async(req, res) => {
 
         const sql = `SELECT id FROM scheduler.user WHERE name = $1 AND phonenumber = $2`;
         const data = await pool.query(sql, [name, phoneNumber]);
-
         const row = data.rows
 
         if(row.length === 0){
@@ -48,7 +47,6 @@ router.get('/find-password', async(req, res) => {
 
         const sql = `SELECT password FROM scheduler.user WHERE id = $1 AND name = $2 AND phoneNumber = $3`;
         const data = await pool.query(sql, [id, name, phoneNumber]);
-
         const row = data.rows
 
         if(row.length === 0){
@@ -76,28 +74,24 @@ router.get('/:idx', async(req, res) => {
     }
 
     try {
-        // DB통신
+
         const sql = `SELECT * FROM scheduler.user WHERE idx = $1`;
         const data = await pool.query(sql, [userIdx]);
-
-        // DB 후처리
         const row = data.rows
 
         if (row.length === 0) {
             throw new Error("회원정보가 존재하지 않습니다.");
         }
         
-        // 결과 설정
         result.success = true;
         result.message = "특정 user 정보 조회 성공";
         result.data = row[0];
 
-    } catch (e) {
-        result.message = e.message;
+    } catch (err) {
+        result.message = err.message;
     } finally {
         res.send(result);
     }
-
 });
 
 // 내 회원 정보 수정
@@ -118,15 +112,44 @@ router.put('/', async(req, res) => {
             RETURNING *; -- 수정된 정보를 반환
         `;
         const data = await pool.query(sql, [password, name, phoneNumber, email, address, 1]);
-
         const row = data.rows
 
         result.success = true;
         result.message = "내 정보 수정 성공";
         result.data = row[0];
         
-    } catch(e) {
-        result.message = e.message;
+    } catch(err) {
+        result.message = err.message;
+    } finally {
+        res.send(result);
+    }
+});
+
+// 회원가입
+router.post('/', async(req, res) => {
+    const { id, password, name, phoneNumber, email, address } = req.body
+    const result = {
+        "success" : false,
+        "message" : "",
+        "data" : null
+    }
+
+    try {
+
+        const sql = `
+            INSERT INTO scheduler.user (id, password, name, phonenumber, email, address) 
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+        `;
+        const data = await pool.query(sql, [id, password, name, phoneNumber, email, address]);
+        const row = data.rows
+
+        result.success = true;
+        result.message = "회원가입 성공";
+        result.data = row[0];
+
+    } catch(err) {
+        result.message = err.message;
     } finally {
         res.send(result);
     }
